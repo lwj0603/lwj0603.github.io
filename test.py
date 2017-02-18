3c16277ab29c19fd01d34a503ad1cfe0b6004c35
Thare_Lam <Thare@Thare-Lam.local>
2017-02-18 17:19:08 +0800

    update

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-a = '<a target="blank" href="http://olk81jnmn.bkt.clouddn.com/00{index}.jpg"><img src="http://olk81jnmn.bkt.clouddn.com/00{index}.jpg"></a>'
-for i in range(0, 10):
-    print a.format(index=i+1)
+a = '<a target="blank" href="http://olkcblu0m.bkt.clouddn.com/00{index}.jpg"><img src="http://olkcblu0m.bkt.clouddn.com/00{index}.jpg"></a>'
+for i in range(1, 12):
+    print a.format(index=i)
